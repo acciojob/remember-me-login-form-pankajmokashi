708d89984255f9d4addfaae66d667a845d147945
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-05-23 16:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 //your JS code here. If required.
 const form = document.querySelector("form")
+const formElements = Array.from(form.elements);
+formElements.forEach(element => {
+	console.log(element);
+});
+let result;
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", function (event) => {
 	event.preventDefault()
-
 	
+	let name = formElements[0]
+	let password = formElements[1]
+	let check = formElements[2]
+
+	if(check.checked === true){
+		result = {
+			name: "${name.value}",
+			password: "${password.value}"
+		}
+		localStorage.setItem("data", result)
+	}
 })
